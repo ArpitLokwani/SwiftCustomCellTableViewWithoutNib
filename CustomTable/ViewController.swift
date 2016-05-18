bfed8c0d16ee30894204cfd068b87a05c11e1778
emb_arpit <arpit.lokwani@embitel.com>
2016-05-18 12:56:56 +0530

    Initial Commit

---
+++ CustomTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomTable
+//
+//  Created by Arpit Lokwani on 5/18/16.
+//  Copyright © 2016 Embitel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
